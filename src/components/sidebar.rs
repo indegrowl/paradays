4df9d58b5baa8d9d5848e2d00a74282595d940e7
Indegrowl <indegrowl.mail@gmail.com>
2024-10-20 08:02:48 +0530

    Added Navbar

--- src/components/sidebar.rs
+++ src/components/sidebar.rs
@@ -7,40 +7,43 @@ pub fn Sidebar() -> Element {
     let mut is_open = use_signal(|| false);
 
     rsx! {
-        svg {
-            class: "fixed top-2 left-2 visible sm:invisible",
-            "stroke-width": "2",
-            "stroke": "{theme_context().text_color}",
-            width: "24",
-            "stroke-linecap": "round",
-            "stroke-linejoin": "round",
-            "viewBox": "0 0 24 24",
-            "xmlns": "http://www.w3.org/2000/svg",
-            "fill": "none",
-            height: "24",
-            onclick: move |_| {is_open.set(true)},
-            line {
-                "x2": "20",
-                "x1": "4",
-                "y1": "12",
-                "y2": "12"
-            }
-            line {
-                "y1": "6",
-                "x2": "20",
-                "y2": "6",
-                "x1": "4"
-            }
-            line {
-                "y1": "18",
-                "x2": "20",
-                "y2": "18",
-                "x1": "4"
-            }
-        },
+        nav {
+            class:"fixed top-0 left-0 p-2 visible lg:invisible border-b w-full",
+            style: "border-color: {theme_context().border_color};",
+            svg {
+                "stroke-width": "2",
+                "stroke": "{theme_context().text_color}",
+                width: "24",
+                "stroke-linecap": "round",
+                "stroke-linejoin": "round",
+                "viewBox": "0 0 24 24",
+                "xmlns": "http://www.w3.org/2000/svg",
+                "fill": "none",
+                height: "24",
+                onclick: move |_| {is_open.set(true)},
+                line {
+                    "x2": "20",
+                    "x1": "4",
+                    "y1": "12",
+                    "y2": "12"
+                }
+                line {
+                    "y1": "6",
+                    "x2": "20",
+                    "y2": "6",
+                    "x1": "4"
+                }
+                line {
+                    "y1": "18",
+                    "x2": "20",
+                    "y2": "18",
+                    "x1": "4"
+                }
+            },
+        }
         aside {
-
-            class:format!("fixed bg-black top-0 left-0 z-40 w-64 h-screen transition-transform {} sm:translate-x-0", if !is_open() {
+            style: "border-color: {theme_context().border_color}; background-color: {theme_context().background_color}",
+            class:format!("fixed top-0 left-0 z-40 w-64 h-screen transition-transform {} lg:translate-x-0 border-r", if !is_open() {
                 "-translate-x-full"
             } else {
                 "-translate-x-0"
